4728d666f2abf1b4727b844e49f62545bff5245c
Daniele Dona <d.dona01@gmail.com>
2024-09-17 23:15:44 +0200

    Added function login()

--- js/login.js
+++ js/login.js
@@ -5,7 +5,25 @@ function checkMsgUrl() {
   if (msg) {
     msgBox.innerHTML = msg;
   } else {
-    document.getElementById('msgBox').style.display = 'none';
+    document.getElementById("msgBox").style.display = "none";
+  }
+}
 
+function login(event) {
+  event.preventDefault();
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  let name = document.getElementById("name");
+
+  let user = users.find(
+    (user) => user.email == email.value && user.password == password.value
+  );
+
+  if (user) {
+    // document.getElementById('logoAnimation').classList.add('paused');
+    console.log("User gefunden!");
+    window.location.href = `/html/summary.html?msg=${name.value}`;
+  } else {
+    console.log("Kein User gefunden!");
   }
 }
--- js/sign_up.js
+++ js/sign_up.js
@@ -1,18 +1,13 @@
-let users = [];
+let users = [{"name": "Daniel","email": "a@a", "password": "123"}];
 
 function addUser() {
     let  name = document.getElementById('name');
     let  email = document.getElementById('email');
     let  password = document.getElementById('password');
-    // let  confirmPassword = document.getElementById('confirmPassword');
 
     users.push({"name": name.value,"email": email.value, "password": password.value,});
     console.log(users);
     window.location.href = "login.html?msg=Du hast dich erfolgreich registriert!";
-    // name.value  = '';
-    // email.value = '';
-    // password.value = '';
-    // confirmPassword.value = '';
 }
 
 
