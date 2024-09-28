c92b28507c7afd02bbee8866dff70dcc15273cc1
Daniele Dona <d.dona01@gmail.com>
2024-09-28 17:44:14 +0200

    Function updateHtml

--- js/board.js
+++ js/board.js
@@ -3,30 +3,67 @@ let todos = [];
 const BASE_URL =
   "https://joinremotestorage-c8226-default-rtdb.europe-west1.firebasedatabase.app/";
 
-function onload() {
+async function onload() {
+    await loadTodosArray();
+    updateHtml();
+
+
     // addTodo("Template", "Erstellung HTML Template", "Lars", "2017-01-01", "toDo", "technical task",);
     // addTodo("Recipe", "Erstellung neuen Rezept", "", "2017-01-01", "inProgress", "user story",);
     // addTodo("Kochwelt", "Architecture", "Peter", "2017-03-01", "done", "user story",); 
 }
 
-
 // function addTodo(title, description, assignedTo, dueDate, swimlane, task_category) {
 //     postData("/todos", { title: title, description: description, assignedTo: assignedTo, dueDate: dueDate, swimlane: swimlane, task_category: task_category, });
 // }
 
-// async function postData(path="", data={}){
-//     let response = await fetch(BASE_URL + path + ".json", {
-//         method: "POST",
-//         header: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(data)
-//     });
-//     return responseToJson = await response.json();
-// }
+async function postData(path="", data={}){
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "POST",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    return responseToJson = await response.json();
+}
 
 
+// fills up the firebase to todos array
+async function loadTodosArray() {
+  let todosResponse = await getAllUsers("todos");
+  let todoKeysArray = Object.keys(todosResponse);
+  // console.log("todosResponse ", todosResponse);
+  // console.log("todoKeysArray", todoKeysArray);
+  
+  for (let i = 0; i < todoKeysArray.length; i++) {
+    todos.push({
+        id: todoKeysArray[i],
+        ...todosResponse[todoKeysArray[i]],
+      
+    },);
+  };
+  console.log("todos", todos);
+};
 
+async function getAllUsers(path){
+  let response = await fetch(BASE_URL + path + ".json");
+  let responseAsJson = await response.json();
+//   console.log("responseAsJson", responseAsJson);
+  return responseAsJson;
+}
+
+function updateHtml() {    
+    let toDoColumn = todos.filter(t => t['swimlane'] == 'toDo');
+    document.getElementById('toDoContent').innerHtml = '';
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+v
+
+}
 
 // Fill up empty content section 
 function init() {
